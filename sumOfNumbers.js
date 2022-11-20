23cd2db3711c28b8d6e8c3ce4f0c8b14bca92f8d
dreadmurkat <arjun@kanaparthi.net>
2022-11-20 13:55:29 -0800

    factors

--- sumOfNumbers.js
+++ sumOfNumbers.js
@@ -19,11 +19,11 @@ return bothSums ;
 
 var numbers = [111, 222, 333, 444];
 var sums = sumOfEvenAndOddNumbers(numbers);
-console.log("sum of even numbers =" + sums[0]);
-console.log("sum of odd numbers =" + sums[1]);
+console.log("sum of even numbers = " + sums[0]);
+console.log("sum of odd numbers = " + sums[1]);
 
     
-
+    
 
 
 
