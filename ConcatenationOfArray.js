c7383e2d3e124cc615ad5ab659e1fdb0dc264776
dreadmurkat <arjun@kanaparthi.net>
2023-02-19 21:37:54 -0800

    base implimentation of number Frequency

---
+++ ConcatenationOfArray.js
@@ -0,0 +1,27 @@
+//array1 is a set of values
+ var array1 = [1, 2, 1];
+
+function Concatenation (array1) {
+    var array2 = []
+     // copy each value in array1 to array2 until they have equal values
+    for(i = 0; i <= array1.length; i++)    
+   if(array1 = i === true)
+    array2.push(i)
+    
+    
+    return array2
+    
+}
+
+
+
+var finalArray = Concatenation (array1)
+
+console.log(finalArray)
+
+
+
+
+//array2 should have array1 in it
+//then array1 should copy again in array2
+
---
+++ mathFuntion.js
@@ -0,0 +1,14 @@
+//absolute value of given number
+function absolute(x) {
+    var absVal = Math.abs(x);
+    
+
+return absVal ;
+
+}
+var x = 0
+var output = absolute(x);
+
+console.log(absolute(-5));
+console.log(absolute(-15));
+console.log(absolute(50));
